62c9d01ac95f0e76b2bc4305abdebcfa6b9bed7a
yannis216 <yannisbeuke@googlemail.com>
2016-09-09 14:49:10 +0200

    added functionality for event detail

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^impressum/$', views.impressum, name='impressum'),
     url(r'^datenschutz/$', views.datenschutz, name='datenschutz'),
     url(r'^events/$', views.events, name='events'),
+    url(r'^events_detail_(?P<pk>\d+)/$', views.event_detail, name='event_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Event
 from django.utils import timezone
 import datetime
@@ -18,3 +18,7 @@ def events(request):
     future_events= Event.objects.filter(end_date__gte=datetime.date.today()).order_by('start_date')
     past_events=Event.objects.filter(end_date__lte=datetime.date.today()).order_by('start_date')
     return render(request, 'home/events.html', {'future_events': future_events, 'past_events': past_events})
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    return render(request, 'home/event_detail.html', {'event': event})
