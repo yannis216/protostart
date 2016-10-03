2d21714ab0c5946ac5f2833b50f10886743dd31a
yannis216 <yannisbeuke@googlemail.com>
2016-10-03 09:51:05 +0200

    ckeditor successfully implemented

--- home/models.py
+++ home/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField(help_text="Dieser Text wird in der Übersicht aller Events eventuell abgeschnitten (Die ersten 100 Wörter) präsentiert und nur auf der Detailansicht vollständig angezeigt. Die wichtigsten Fakten also am Anfang")
+    description = RichTextField(help_text="Dieser Text wird in der Übersicht aller Events eventuell abgeschnitten (Die ersten 100 Wörter) präsentiert und nur auf der Detailansicht vollständig angezeigt. Die wichtigsten Fakten also am Anfang")
     location = models.CharField(max_length=100, help_text="Bitte in dem Format: NameDesVeranstaltungsortes, Straße, Stadt, Beispiel: Protostart Firmenzentrale, Hausdorffstraße 34, Bonn")
     start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(default=timezone.now, help_text="WICHTIG! Hier zumindest einen Schätzwert angeben. Genaue Uhrzeit und Datum werden eh nirgendwo angezeigt, aber Events die ein falsches Enddatum haben werden eventuell in einer falschen kategorie , z.B. Vergangene und nicht zukünftige Events angezeigt")
